d6abd0344c0ff408603c893685301d169999354f
Taylor Monacelli <taylormonacelli@gmail.com>
2023-05-29 20:42:51 -0700

    Remove test command and add --log-level

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,18 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bufio"
+	"crypto/sha256"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"strings"
 
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -25,7 +34,9 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		doit()
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -40,24 +51,30 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.nightrover.yaml)")
-
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().String("log-level",
+		"info", "Log level (trace, debug, info, warn, error, fatal, panic)",
+	)
+
+	consoleWriter := zerolog.ConsoleWriter{
+		Out: os.Stderr,
+		PartsExclude: []string{
+			zerolog.TimestampFieldName,
+		},
+		FormatLevel: func(i interface{}) string {
+			return strings.ToUpper(fmt.Sprintf("[%s]", i))
+		},
+	}
+
+	log.Logger = log.Output(consoleWriter)
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
-		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
@@ -69,8 +86,169 @@ func initConfig() {
 
 	viper.AutomaticEnv() // read in environment variables that match
 
-	// If a config file is found, read it in.
+	viper.BindPFlag("log-level", rootCmd.Flags().Lookup("log-level"))
+
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
+
+	setupLogging()
+}
+
+func setupLogging() {
+	logLevel := viper.GetString("log-level")
+
+	switch logLevel {
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case "fatal":
+		zerolog.SetGlobalLevel(zerolog.FatalLevel)
+	case "panic":
+		zerolog.SetGlobalLevel(zerolog.PanicLevel)
+	default:
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
+}
+
+type Setting struct {
+	SourcePath     string
+	SourceFile     *os.File
+	TempTargetPath string
+	TempTargetFile *os.File
+}
+
+func doit() {
+	paths := []string{}
+
+	switch os := runtime.GOOS; os {
+	case "darwin", "linux":
+		// for testing
+		paths = append(paths, "data/settings.xml.bak")
+		paths = append(paths, "data/settings.xml")
+	case "windows":
+		paths = append(paths, "C:\\ProgramData\\Streambox\\SpectraUI\\settings.xml.bak")
+		paths = append(paths, "C:\\ProgramData\\Streambox\\SpectraUI\\settings.xml")
+	default:
+		log.Error().Msgf("Running on %s.\n", os)
+	}
+
+	settings := []Setting{}
+	sourcePath := ""
+	for _, pathStr := range paths {
+		sourcePath, _ = filepath.Abs(pathStr)
+		settings = append(settings, Setting{SourcePath: sourcePath})
+	}
+
+	for _, setting := range settings {
+		updateSettings(setting)
+	}
+}
+
+func updateSettings(s Setting) {
+	s.TempTargetPath = filepath.Join(os.TempDir(), "nightrover_")
+	if err := createTemporaryFile(&s); err != nil {
+		log.Error().Msgf("Error creating temporary file %s: %v\n", s.TempTargetPath, err)
+		return
+	}
+	defer os.Remove(s.TempTargetPath)
+
+	log.Debug().Msgf("Temporary file %s created to update %s\n",
+		s.TempTargetFile.Name(),
+		s.SourcePath,
+	)
+
+	if err := openSourceFile(&s); err != nil {
+		log.Error().Msgf("Failed to open file %s: %v\n", s.SourcePath, err)
+		return
+	}
+	defer s.SourceFile.Close()
+
+	pattern := regexp.MustCompile(`video_3d *= *"[^"]+" ?`)
+	scanner := bufio.NewScanner(s.SourceFile)
+
+	if err := processLines(scanner, pattern, s.TempTargetFile); err != nil {
+		log.Error().Msgf("Error processing lines: %v\n", err)
+		return
+	}
+
+	if sameChecksum, err := compareChecksums(s.TempTargetFile.Name(), s.SourceFile.Name()); err != nil {
+		log.Debug().Msgf("Error comparing checksums: %v", err)
+		return
+	} else if sameChecksum {
+		log.Debug().Msg("The files have the same checksum, no changes made.")
+		return
+	}
+
+	if err := replaceOriginalFile(s.TempTargetFile.Name(), s.SourceFile.Name()); err != nil {
+		log.Error().Msgf("Failed to replace the original file: %v\n", err)
+		return
+	}
+
+	log.Info().Msgf("%s updated", s.SourcePath)
+}
+
+func createTemporaryFile(s *Setting) error {
+	var err error
+	s.TempTargetFile, err = os.CreateTemp(filepath.Dir(s.TempTargetPath), filepath.Base(s.TempTargetPath))
+	return err
+}
+
+func openSourceFile(s *Setting) error {
+	var err error
+	s.SourceFile, err = os.Open(s.SourcePath)
+	return err
+}
+
+func processLines(scanner *bufio.Scanner, pattern *regexp.Regexp, tempTargetFile *os.File) error {
+	for scanner.Scan() {
+		line := scanner.Text()
+		newLine := pattern.ReplaceAllString(line, "")
+
+		if _, err := fmt.Fprintln(tempTargetFile, newLine); err != nil {
+			return err
+		}
+	}
+
+	return scanner.Err()
+}
+
+func compareChecksums(file1, file2 string) (bool, error) {
+	sum1, err := calculateSHA256(file1)
+	if err != nil {
+		return false, err
+	}
+
+	sum2, err := calculateSHA256(file2)
+	if err != nil {
+		return false, err
+	}
+
+	return compareSums(sum1, sum2), nil
+}
+
+func calculateSHA256(file string) ([]byte, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, f); err != nil {
+		return nil, err
+	}
+
+	return hash.Sum(nil), nil
+}
+
+func compareSums(sum1, sum2 []byte) bool {
+	return fmt.Sprintf("%x", sum1) == fmt.Sprintf("%x", sum2)
+}
+
+func replaceOriginalFile(tempTargetFile, sourceFile string) error {
+	return os.Rename(tempTargetFile, sourceFile)
 }
